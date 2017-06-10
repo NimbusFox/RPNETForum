e1145ccdc08520593fb5235a2304daa2098ccfac
nimbusfox <simonbailey10@hotmail.com>
2017-06-10 22:43:08 +0100

    Build issue
    Fixed issue where a proj file had corrupt text.
    
    Fixed issue where js and css bundles were not being seen

--- RPNETForum/App_Start/BundleConfig.cs
+++ RPNETForum/App_Start/BundleConfig.cs
@@ -5,9 +5,9 @@ namespace RPNETForum {
     public class BundleConfig {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/js/dependencies.js").IncludeDirectory("~/Scripts/dependencies", "*.js"));
+            bundles.Add(new ScriptBundle("~/js/dependencies").IncludeDirectory("~/Scripts/dependencies", "*.js"));
 
-            bundles.Add(new StyleBundle("~/site.css").IncludeDirectory("~/Content/base", "*.css"));
+            bundles.Add(new StyleBundle("~/css/site.css").IncludeDirectory("~/Content/base", "*.css"));
         }
     }
 }
